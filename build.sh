64cfb9fad948da5628c3b0f8279afe089a5739e5
Sarthak Roy <sarthakroy2002@gmail.com>
2023-01-30 10:44:24 +0530

    build-script: Update toolchains and Nuke AnyKernel folder before building
    Change-Id: I497f9332307d87d8d537c9a8f4f3b0716e585e07
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 function compile() 
 {
-
+rm -rf AnyKernel
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=neolit
 export KBUILD_BUILD_USER="sarthakroy2002"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-7612306 clang
-git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 make O=out ARCH=arm64 RMX2020_defconfig
 
@@ -32,7 +32,6 @@ git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Test-OSS-KERNEL-RMX2020-NEOLIT.zip *
-#curl --upload-file Test-OSS-KERNEL-RMX2020-NEOLIT.zip https://transfer.sh/
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet Test-OSS-KERNEL-RMX2020-NEOLIT.zip
 }
